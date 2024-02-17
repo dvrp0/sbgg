6eea34064feaf77607fe67354142b5cb8da6ed5e
DVRP <daveripper0020@gmail.com>
2024-02-17 23:39:01 +0900

    게임 기록 그리드 열이 자동 증가하도록 변경

--- frontend/src/components/Match.svelte
+++ frontend/src/components/Match.svelte
@@ -2,8 +2,8 @@
     export let won: boolean | undefined = undefined;
 </script>
 
-<div class="w-full h-0 rounded pb-[100%] transition-colors duration-300
-    {won === undefined ? "border-[3px] border-gray-100 border-solid" :
+<div class="w-full h-0 rounded pb-[100%] transition-all duration-300 hover:scale-[0.85]
+    {won === undefined ? "border-2 border-gray-100 border-solid" :
         won ? "bg-platinum" : "bg-gray-100"}">
 
 </div>
