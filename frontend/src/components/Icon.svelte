6b8ac4651895fdb49dca1484812d23e81eefaaa4
DVRP <daveripper0020@gmail.com>
2024-02-18 16:37:52 +0900

    Add Icon component

---
+++ frontend/src/components/Icon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let kind: "base" | "sparkle" | "star" | "fire";
+    export let big = false;
+    export let color = "gray-900";
+
+    let src = `src/assets/images/${kind}.svg`;
+    let backgroundColor = `bg-${color}`;
+</script>
+
+<div class="{big ? "w-[40px] h-[40px]" : "w-[20px] h-[20px]"} {backgroundColor}"
+    style="--src: url({src});">
+    <img class="opacity-0" {src} alt="Icon" />
+</div>
+
+<style>
+    div {
+        -webkit-mask: var(--src) no-repeat center;
+        mask: var(--src) no-repeat center;
+    }
+</style>
