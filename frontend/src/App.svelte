5a69fc9dbac77ce67be934b47152a7faa1cdc5c2
DVRP <daveripper0020@gmail.com>
2024-02-19 02:56:15 +0900

    화면과 유저 데이터 일부 연동

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import Icon from "$components/Icon.svelte";
     import Match from "$components/Match.svelte";
     import Skeleton from "$components/Skeleton.svelte";
-    import { user } from "$lib/store";
-    import { GetUserData } from "$wails/App";
+    import { TAILWIND, user } from "$lib/store";
+    import { getUserData } from "$lib/utils";
 
     let isMounted = false;
 
+    $: won = $user.rankedWon;
+    $: lose = $user.rankedPlayed - $user.rankedWon;
+    $: winRate = ($user.rankedWon / $user.rankedPlayed * 100).toFixed(1);
+
     onMount(async () => {
-        $user = await GetUserData();
+        $user = await getUserData();
+        document.documentElement.style.setProperty("--league", $TAILWIND.theme.colors[$user.userLeague]);
 
         isMounted = true;
     });
@@ -23,16 +29,25 @@
             <Skeleton kind="body" />
         </div>
     {:else}
-        <div class="flex">
-            <span class="font-black">dvrp</span><span class="text-gray-500">#1001413882</span>
+        <div class="flex items-center">
+            <span class="font-black">{$user.username}</span><span class="text-gray-500">#{$user.userId}</span>
+            <div class="mx-2xs">
+                <Icon kind="sparkle" />
+            </div>
+            <Icon kind="base" /><span class="ml-3xs">{$user.userLevel}</span>
         </div>
-        <div class="flex mt-xs">
-            <h1 class="text-platinum">Platinum 2</h1>
+        <div class="flex mt-xs items-center">
+            <h1 class="text-league">{$user.userLeague} {$user.userDivision}</h1>
+            <div class="ml-md grid grid-cols-4 gap-2xs">
+                {#each Array($user.userStars) as _}
+                    <Icon kind="star" big color="bg-league" />
+                {/each}
+            </div>
         </div>
         <div class="grid grid-cols-[repeat(4,auto)] grid-rows-2 w-fit mt-sm gap-y-2xs gap-x-4xl">
-            <h2>116</h2>
-            <h2>13</h2>
-            <h2>89.4%</h2>
+            <h2>{won}</h2>
+            <h2>{lose}</h2>
+            <h2>{winRate}%</h2>
             <h2>15</h2>
             <span>Wins</span>
             <span>Loses</span>
@@ -40,8 +55,7 @@
             <span>Avg. turns</span>
         </div>
     {/if}
-    <div class="border-solid border-[1px] border-gray-100 -mx-4xl my-md" />
-    <div class="flex justify-between mt-xs">
+    <div class="flex justify-between mt-4xl">
         <span>Matches</span>
         <span class="text-gray-500">Share</span>
     </div>
