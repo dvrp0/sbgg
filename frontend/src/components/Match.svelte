577feab76f80a3b265748a5884f32b9e0f4d1a40
DVRP <daveripper0020@gmail.com>
2024-02-19 02:47:43 +0900

    현재 리그를 가리키는 theme color 추가

--- frontend/src/components/Match.svelte
+++ frontend/src/components/Match.svelte
@@ -4,6 +4,6 @@
 
 <div class="w-full h-0 rounded pb-[100%] transition-all duration-300 hover:scale-[0.85]
     {won === undefined ? "border-2 border-gray-100 border-solid" :
-        won ? "bg-platinum" : "bg-gray-100"}">
+        won ? "bg-league" : "bg-gray-100"}">
 
 </div>
